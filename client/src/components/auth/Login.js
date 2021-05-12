29d29a598563847794c40f98fe58a8ebaabcf733
gaurav3210 <gs070599@gmail.com>
2021-05-13 01:48:31 +0530

    Created Register && Login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,64 @@
-import React from "react";
-
+import React,{Fragment, useState } from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+    const {email,password} = formData;
+    const onChange = e =>setFormData({...formData,[e.target.name]: e.target.value})
+    const onSubmit = async e =>{
+        e.preventDefault();
+
+
+            // const newUser ={
+            //     name,
+            //     email,
+            //     password
+            // }
+            // try{
+            //     const config = {
+            //         headers: {
+            //             'Content-Type': 'application/json'
+            //         }
+            //     }
+            //     const body = JSON.stringify(newUser);
+            //     const res = await axios.post('/api/users',body,config);
+            //     console.log(res.data);
+            //
+            // }catch (e) {
+            //    console.error(e.response.data);
+            // }
+            console.log("SUCCESS");
+
+    }
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+            <form className="form" onSubmit={e=> onSubmit(e)}>
+
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" name="email" value={email} onChange={e=>onChange(e)}/>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password} onChange={e=>onChange(e)}
+                        minLength="6"
+                    />
+                </div>
+
+
+                <input type="submit" className="btn btn-primary" value="Register"/>
+            </form>
+            <p className="my-1">
+                Dont have an account? <Link to="/register">Sign In</Link>
+            </p>
+        </Fragment>
     )
 };
 
